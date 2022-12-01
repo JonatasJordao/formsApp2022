d60bc4f9018678d8a2ac223404d8bb2029987928
JonatasJordao <jonatas.oliveira59@etec.sp.gov.br>
2022-12-01 12:13:48 -0300

    0112
    0112

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  FormRegistro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.FormRegistro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required])],
+      senha: ['', Validators.compose([Validators.required])],
+      confirmaSenha: ['', Validators.compose([Validators.required])]
+
+    });
+   }
 
   ngOnInit() {
   }
